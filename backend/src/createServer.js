778edfc3d7bd42728f4e0652b1f5c637f7bbac59
Dmitriy Beglov <ditransler@gmail.com>
2019-02-18 21:03:33 +0400

    Created GraphQL Yoga Server

---
+++ backend/src/createServer.js
@@ -0,0 +1,23 @@
+const { GraphQLServer } = require('graphql-yoga');
+const Query = require('./resolvers/Query');
+const Mutation = require('./resolvers/Mutation');
+const db = require('./db');
+
+// Create GraphQL Yoga Server
+
+function createServer () {
+    return new GraphQLServer({
+        typeDefs: 'src/schema.graphql',
+        resolvers: {
+            Mutation,
+            Query
+        },
+        resolverValidationOptions: {
+            requireResolversForResolveType: false
+        },
+        context: req => ({ ...req, db })
+
+    });
+}
+
+module.exports = createServer;
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,18 @@
 // let's go!
+require('dotenv').config({ path: 'variables.env' });
+const createServer = require('./createServer');
+const db = require('./db');
+
+const server = createServer();
+
+// TODO: Use express middleware to handle cookies (JWT)
+// TODO: Use express middleware to populate current user
+
+server.start({
+    cors: {
+        credentials: true,
+        origin: process.env.FRONTEND_URL
+    }
+}, deets => {
+    console.log(`The server is now running on the http://localhost:${deets.port}`)
+});
