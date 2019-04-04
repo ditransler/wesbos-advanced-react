013a8b9cbdcd737cea57ef25643aea194228277f
Dmitriy Beglov <ditransler@gmail.com>
2019-04-04 22:17:28 +0400

    Added Signup on backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,13 @@
 // let's go!
+const cookieParser = require('cookie-parser');
 require('dotenv').config({ path: 'variables.env' });
 const createServer = require('./createServer');
 const db = require('./db');
 
 const server = createServer();
 
-// TODO: Use express middleware to handle cookies (JWT)
+server.express.use(cookieParser());
+
 // TODO: Use express middleware to populate current user
 
 server.start(
--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const Mutations = {
     async createItem(parent, args, ctx, info) {
         // TODO: Check if they are logged in
@@ -40,6 +43,29 @@ const Mutations = {
         // 3. Delete it!
         return ctx.db.mutation.deleteItem({ where }, info);
 
+    },
+    async signup(parent, args, ctx, info) {
+        // lowercase their email
+        args.email = args.email.toLocaleLowerCase();
+        // hash their password
+        const password = await bcrypt.hash(args.password, 10);
+        // create the user in the database
+        const user = ctx.db.mutation.createUser({
+            data: {
+                ...args,
+                password,
+                permissions: {set: ['USER']}
+            }
+        }, info);
+        // create the JWT token for them
+        const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+        // set the jwt as a cookie on the response
+        ctx.response.cookie('token', token, {
+            httpOnly: true,
+            maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
+        });
+        // finally return the user to the browser
+        return user;
     }
 };
 
