f28498e9a23c33155dfa1633b3ea97ace952f058
Dmitriy Beglov <ditransler@gmail.com>
2019-06-15 08:49:34 +0400

    Added Reset Password

--- frontend/pages/signup.js
+++ frontend/pages/signup.js
@@ -1,20 +1,20 @@
 import Signup from '../components/Signup';
 import Signin from '../components/Signin';
+import RequestReset from '../components/RequestReset';
 import styled from 'styled-components';
 
 const Columns = styled.div`
-    display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
-    grid-gap: 20px;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-gap: 20px;
 `;
 
-const SignupPage = (props) => {
-    return (
-        <Columns>
-            <Signup />
-            <Signin />
-        </Columns>
-    );
-};
+const SignupPage = props => (
+    <Columns>
+        <Signup />
+        <Signin />
+        <RequestReset />
+    </Columns>
+);
 
 export default SignupPage;
