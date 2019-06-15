4c1c64fbabebcb983922dae5eb50021e17278e09
Dmitriy Beglov <ditransler@gmail.com>
2019-06-15 15:22:27 +0400

    Locked down DeleteItem persmissions

--- frontend/components/DeleteItem.js
+++ frontend/components/DeleteItem.js
@@ -8,20 +8,20 @@ const DELETE_ITEM_MUTATION = gql`
         deleteItem(id: $id) {
             id
         }
-    }  
+    }
 `;
 
 class DeleteItem extends Component {
     update = (cache, payload) => {
-        // manually update the cache on the client so it matches the server
+        // manually update the cache on the client, so it matches the server
         // 1. Read the cache for the items we want
         const data = cache.readQuery({ query: ALL_ITEMS_QUERY });
+        console.log(data, payload);
         // 2. Filter the deleted item out of the page
-        data.items = data.items.filter((item) => item.id !== payload.data.deleteItem.id);
+        data.items = data.items.filter(item => item.id !== payload.data.deleteItem.id);
         // 3. Put the items back!
-        cache.writeQuery({ query: ALL_ITEMS_QUERY, data })
+        cache.writeQuery({ query: ALL_ITEMS_QUERY, data });
     };
-
     render() {
         return (
             <Mutation
@@ -31,14 +31,15 @@ class DeleteItem extends Component {
             >
                 {(deleteItem, { error }) => (
                     <button
-                        type="button"
                         onClick={() => {
                             if (confirm('Are you sure you want to delete this item?')) {
-                                deleteItem();
+                                deleteItem().catch(err => {
+                                    alert(err.message);
+                                });
                             }
                         }}
                     >
-                        { this.props.children }
+                        {this.props.children}
                     </button>
                 )}
             </Mutation>
