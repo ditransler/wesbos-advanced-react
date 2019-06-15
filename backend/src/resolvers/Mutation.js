0da05fad3b7668ce0f097dc9e933762b34ef6767
Dmitriy Beglov <ditransler@gmail.com>
2019-06-15 08:54:50 +0400

    Small fixes in Mutation

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -39,63 +39,63 @@ const Mutations = {
     },
     async deleteItem(parent, args, ctx, info) {
         const where = { id: args.id };
-        // 1. Find the item
-        const item = await ctx.db.query.item({ where }, `{ id, title }`);
-        // 2. Check if they own that item or have the permissions
+        // 1. find the item
+        const item = await ctx.db.query.item({ where }, `{ id title}`);
+        // 2. Check if they own that item, or have the permissions
         // TODO
         // 3. Delete it!
         return ctx.db.mutation.deleteItem({ where }, info);
-
     },
     async signup(parent, args, ctx, info) {
         // lowercase their email
-        args.email = args.email.toLocaleLowerCase();
+        args.email = args.email.toLowerCase();
         // hash their password
         const password = await bcrypt.hash(args.password, 10);
         // create the user in the database
-        const user = await ctx.db.mutation.createUser({
-            data: {
-                ...args,
-                password,
-                permissions: {set: ['USER']}
-            }
-        }, info);
+        const user = await ctx.db.mutation.createUser(
+            {
+                data: {
+                    ...args,
+                    password,
+                    permissions: { set: ['USER'] },
+                },
+            },
+            info
+        );
         // create the JWT token for them
         const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
-        // set the jwt as a cookie on the response
+        // we set the jwt as a cookie on the response
         ctx.response.cookie('token', token, {
             httpOnly: true,
             maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
         });
-        // finally return the user to the browser
+        // finally we return the user to the browser
         return user;
     },
     async signin(parent, { email, password }, ctx, info) {
-        // check if there is a user with this email
-        const user = await ctx.db.query.user({ where: { email }});
+        // 1. check if there is a user with that email
+        const user = await ctx.db.query.user({ where: { email } });
         if (!user) {
-            throw new Error(`No such user with the ${email} address`);
+            throw new Error(`No such user found for email ${email}`);
         }
-        // check if password is correct
-        const isPasswordValid = await bcrypt.compare(password, user.password);
-        if (!isPasswordValid) {
-            throw new Error('Invalid password');
+        // 2. Check if their password is correct
+        const valid = await bcrypt.compare(password, user.password);
+        if (!valid) {
+            throw new Error('Invalid Password!');
         }
-        // generate JWT token
-        const token = jwt.sign({ userId: user.id}, process.env.APP_SECRET);
-        // set the cookie with the token
+        // 3. generate the JWT Token
+        const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+        // 4. Set the cookie with the token
         ctx.response.cookie('token', token, {
             httpOnly: true,
-            maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
+            maxAge: 1000 * 60 * 60 * 24 * 365,
         });
-        // return the user
+        // 5. Return the user
         return user;
     },
     signout(parent, args, ctx, info) {
         ctx.response.clearCookie('token');
-        return {
-            message: 'Goodbye!'
-        };
+        return { message: 'Goodbye!' };
     },
     async requestReset(parent, args, ctx, info) {
         // 1. Check if this is a real user
@@ -157,7 +157,7 @@ const Mutations = {
         // 7. Set the JWT cookie
         ctx.response.cookie('token', token, {
             httpOnly: true,
-            maxAge: 1000 * 60 * 60 * 24 * 365,
+            maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
         });
         // 8. return the new user
         return updatedUser;
