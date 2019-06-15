a6e369ecfaf74792e814975e7c527e03c6a715bb
Dmitriy Beglov <ditransler@gmail.com>
2019-06-15 14:51:09 +0400

    Started Persmissions Management

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,6 @@
-// let's go!
 const cookieParser = require('cookie-parser');
 const jwt = require('jsonwebtoken');
+
 require('dotenv').config({ path: 'variables.env' });
 const createServer = require('./createServer');
 const db = require('./db');
@@ -9,27 +9,37 @@ const server = createServer();
 
 server.express.use(cookieParser());
 
-// decode JWT so we can get the user Id on each request
+// 1. Decode the JWT so we can get the user Id on each request
 server.express.use((req, res, next) => {
     const { token } = req.cookies;
     if (token) {
         const { userId } = jwt.verify(token, process.env.APP_SECRET);
-        // put the userId onto the request for further requests to access
+        // put the userId onto the req for future requests to access
         req.userId = userId;
     }
     next();
 });
 
-// TODO: Use express middleware to populate current user
+// 2. Create a middleware that populates the user on each request
+server.express.use(async (req, res, next) => {
+    // if they aren't logged in, skip this
+    if (!req.userId) return next();
+    const user = await db.query.user(
+        { where: { id: req.userId } },
+        '{ id, permissions, email, name }'
+    );
+    req.user = user;
+    next();
+});
 
 server.start(
     {
         cors: {
             credentials: true,
-            origin: process.env.FRONTEND_URL
-        }
+            origin: process.env.FRONTEND_URL,
+        },
     },
     deets => {
-        console.log(`The server is now running on http://localhost:${deets.port}`)
+        console.log(`Server is now running on port http://localhost:${deets.port}`);
     }
 );
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,4 +1,5 @@
 const { forwardTo } = require('prisma-binding');
+const { hasPermission } = require('../utils');
 
 const Query = {
     items: forwardTo('db'),
@@ -9,10 +10,25 @@ const Query = {
         if (!ctx.request.userId) {
             return null;
         }
-        return ctx.db.query.user({
-            where: { id: ctx.request.userId }
-        }, info);
-    }
+        return ctx.db.query.user(
+            {
+                where: { id: ctx.request.userId },
+            },
+            info
+        );
+    },
+    async users(parent, args, ctx, info) {
+        // 1. Check if they are logged in
+        if (!ctx.request.userId) {
+            throw new Error('You must be logged in!');
+        }
+        console.log(ctx.request.userId);
+        // 2. Check if the user has the permissions to query all the users
+        hasPermission(ctx.request.user, ['ADMIN', 'PERMISSIONUPDATE']);
+
+        // 2. if they do, query all the users!
+        return ctx.db.query.users({}, info);
+    },
 };
 
 module.exports = Query;
