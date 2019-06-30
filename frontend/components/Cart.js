7c46e22e1c1e589c12a0c5e8da6ccc435aa074eb
Dmitriy Beglov <ditransler@gmail.com>
2019-06-30 21:09:55 +0400

    Created TakeMyMoney.js

--- frontend/components/Cart.js
+++ frontend/components/Cart.js
@@ -10,6 +10,7 @@ import SickButton from './styles/SickButton';
 import CartItem from './CartItem';
 import calcTotalPrice from '../lib/calcTotalPrice';
 import formatMoney from '../lib/formatMoney';
+import TakeMyMoney from './TakeMyMoney';
 
 const LOCAL_STATE_QUERY = gql`
     query {
@@ -49,7 +50,9 @@ const Cart = () => (
                     <ul>{me.cart.map(cartItem => <CartItem key={cartItem.id} cartItem={cartItem} />)}</ul>
                     <footer>
                         <p>{formatMoney(calcTotalPrice(me.cart))}</p>
-                        <SickButton>Checkout</SickButton>
+                        <TakeMyMoney>
+                            <SickButton>Checkout</SickButton>
+                        </TakeMyMoney>
                     </footer>
                 </CartStyles>
             );
