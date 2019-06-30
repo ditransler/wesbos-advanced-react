e80d815c2eb13dedf9193ecf52c1398bb3630002
Dmitriy Beglov <ditransler@gmail.com>
2019-06-30 22:00:27 +0400

    Saved orders to database

--- frontend/components/TakeMyMoney.js
+++ frontend/components/TakeMyMoney.js
@@ -28,17 +28,18 @@ function totalItems(cart) {
 }
 
 class TakeMyMoney extends React.Component {
-    onToken = (res, createOrder) => {
+    onToken = async (res, createOrder) => {
         console.log('On Token Called!');
         console.log(res.id);
         // manually call the mutation once we have the stripe token
-        createOrder({
+        const order = await createOrder({
             variables: {
                 token: res.id,
             },
         }).catch(err => {
             alert(err.message);
         });
+        console.log(order);
     };
     render() {
         return (
@@ -53,7 +54,7 @@ class TakeMyMoney extends React.Component {
                                 amount={calcTotalPrice(me.cart)}
                                 name="Sick Fits"
                                 description={`Order of ${totalItems(me.cart)} items!`}
-                                image={me.cart[0].item && me.cart[0].item.image}
+                                image={me.cart.length && me.cart[0].item && me.cart[0].item.image}
                                 stripeKey="pk_test_7KSnvvG1S2QCm3AUaoyGyurl00pjMgcSGU"
                                 currency="USD"
                                 email={me.email}
