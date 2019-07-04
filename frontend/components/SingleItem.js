bb5c5e7fa2e3064725497869955296083d572ff4
Dmitriy Beglov <ditransler@gmail.com>
2019-07-04 21:37:17 +0400

    Added testing and mocking Apollo queries

--- frontend/components/SingleItem.js
+++ frontend/components/SingleItem.js
@@ -6,64 +6,48 @@ import styled from 'styled-components';
 import Head from 'next/head';
 
 const SingleItemStyles = styled.div`
-    max-width: 1200px;
-    margin: 2rem auto;
-    box-shadow: ${props => props.theme.bs};
-    display: grid;
-    grid-auto-columns: 1fr;
-    grid-auto-flow: column;
-    min-height: 800px;
-    
-    img {
-        width: 100%;
-        height: 100%;
-        object-fit: contain;
-    }
-    
-    .details {
-        margin: 3rem;
-        font-size: 2rem;
-    }
+  max-width: 1200px;
+  margin: 2rem auto;
+  box-shadow: ${props => props.theme.bs};
+  display: grid;
+  grid-auto-columns: 1fr;
+  grid-auto-flow: column;
+  min-height: 800px;
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+  }
+  .details {
+    margin: 3rem;
+    font-size: 2rem;
+  }
 `;
 
 const SINGLE_ITEM_QUERY = gql`
     query SINGLE_ITEM_QUERY($id: ID!) {
         item(where: { id: $id }) {
-            id,
-            title,
-            description,
+            id
+            title
+            description
             largeImage
         }
     }
 `;
-
 class SingleItem extends Component {
     render() {
         return (
-            <Query query={SINGLE_ITEM_QUERY} variables={{
-                id: this.props.id
-            }}>
-                {({ error, loading, data}) => {
-                    if (error) {
-                        return (
-                            <Error error={error} />
-                        );
-                    }
-
-                    if (loading) {
-                        return (
-                            <p>Loading...</p>
-                        );
-                    }
-
-                    if (!data.item) {
-                        return (
-                            <p>No item found for {this.props.id}</p>
-                        );
-                    }
-
-                    const { item } = data;
-
+            <Query
+                query={SINGLE_ITEM_QUERY}
+                variables={{
+                    id: this.props.id,
+                }}
+            >
+                {({ error, loading, data }) => {
+                    if (error) return <Error error={error} />;
+                    if (loading) return <p>Loading...</p>;
+                    if (!data.item) return <p>No Item Found for {this.props.id}</p>;
+                    const item = data.item;
                     return (
                         <SingleItemStyles>
                             <Head>
@@ -83,3 +67,4 @@ class SingleItem extends Component {
 }
 
 export default SingleItem;
+export { SINGLE_ITEM_QUERY };
